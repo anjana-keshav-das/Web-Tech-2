e209468d0b7ef3c797bcaa6ff89beb83082c8c11
Keshav Das <keshavdas@Keshavs-MacBook-Air.local>
2019-11-14 12:34:58 +0530

    Login Signup Complete

--- UserLogin.py
+++ UserLogin.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 import pymongo
 
 app = Flask(__name__)
-api = CORS(app)
+CORS(app)
 
 client = MongoClient('mongodb://localhost:27017/')
 db = client.PizzaDB
@@ -20,62 +20,77 @@ def is_sha1(maybe_sha):
         return False
     return True
 
+
 # Add User
 @app.route("/api/v1/users", methods=['POST'])
 def add_user():
-	login = db.login
-	q = login.find_one({'username': request.json['username']})
-	if q != None:
-		print("User exisits")
-		return jsonify({}), 400
-	else:
-		if is_sha1(request.json['password']):
-			output = {'username': request.json['username'],
-			    'password': request.json['password']}
-			q = login.insert(output)
-			print(output, ": User inserted")
-			return jsonify({}), 201
-		else:
-			print("Bad Request: Add User")
-			return jsonify({}), 400
+    login = db.login
+    q = login.find_one({'username': request.json['username']})
+    if q != None:
+        print("User exisits")
+        return jsonify({}), 400
+    else:
+        if is_sha1(request.json['password']):
+            output = {
+                'username': request.json['username'],
+                'password': request.json['password']
+            }
+            q = login.insert(output)
+            print(output, ": User inserted")
+            return jsonify({}), 201
+        else:
+            print("Bad Request: Add User")
+            return jsonify({}), 400
+
 
 # Remove User
 @app.route("/api/v1/users/<username>", methods=['DELETE'])
 def remove_user(username):
     login = db.login
     details = db.details
-    q = login.remove({'username' : username})
-    if(q['n'] == 0):
+    q = login.remove({'username': username})
+    if (q['n'] == 0):
         print("Bad Request: Remove User")
-        return jsonify({}),400
+        return jsonify({}), 400
     else:
-        print("User:", username,"deleted")
-        q = details.remove({'username' : username})
-        return jsonify({}),200
+        print("User:", username, "deleted")
+        q = details.remove({'username': username})
+        return jsonify({}), 200
+
 
 # Get User for login or get details
 @app.route("/api/v1/users/<username>", methods=['GET'])
 @app.route("/api/v1/users/<username>?details=<val>", methods=['GET'])
 def get_user(username):
     val = request.args.get('details')
-    if(val == None):
+    if (val == None):
         login = db.login
         q = login.find_one({'username': username})
-        if(q==None):
+        if (q == None):
             print("Bad Request: Get User")
-            return jsonify({}),400
+            return jsonify({}), 400
         else:
             print("User found")
-            return jsonify({'username':q['username'], 'password':q['password']}),200
+            return jsonify({
+                'username': q['username'],
+                'password': q['password']
+            }), 200
     else:
         details = db.details
         q = details.find_one({'username': username})
-        if(q==None):
+        if (q == None):
             print("No details found")
-            return jsonify({}),400
+            return jsonify({}), 204
         else:
-            output = {'username':q['username'],'name':q['name'],'contact':q['contact'],'address':q['address'],'email':q['email']}
-            return jsonify(output),200
+            output = {
+                'username': q['username'],
+                'name': q['name'],
+                'contact': q['contact'],
+                'address': q['address'],
+                'email': q['email']
+            }
+            return jsonify(output), 200
+
 
 # Add or update user details or password
 @app.route("/api/v1/users/<username>", methods=['POST'])
@@ -85,36 +100,48 @@ def add_user_details(username):
     login = db.login
     details = db.details
     q = login.find_one({'username': username})
-    if(q==None):
+    if (q == None):
         print("No such user")
-        return jsonify({}),400
-    if(val == None):
+        return jsonify({}), 400
+    if (val == None):
         q = details.find_one({'username': username})
-        if(q==None): #add details
-            res = {'username':username,'name':request.json['name'],'contact':request.json['contact'],'address':request.json['address'],'email':request.json['email']}
+        if (q == None):  #add details
+            res = {
+                'username': username,
+                'name': request.json['name'],
+                'contact': request.json['contact'],
+                'address': request.json['address'],
+                'email': request.json['email']
+            }
             q = details.insert(res)
             print(res, ": details inserted")
             return jsonify({}), 201
         else:
-            q = details.update({'username':username},{'$set':{request.json['field']:request.json['value']}})
-            if(q['n'] == 0):
+            q = details.update(
+                {'username': username},
+                {'$set': {
+                    request.json['field']: request.json['value']
+                }})
+            if (q['n'] == 0):
                 print("Update failed")
-                return jsonify({}),400
+                return jsonify({}), 400
             print("Update successful")
-            return jsonify({}),200
+            return jsonify({}), 200
     else:
         if is_sha1(request.json['password']):
-            q = login.update({'username':username},{'$set':{'password':request.json['password']}})
-            if(q['n'] == 0):
+            q = login.update({'username': username},
+                             {'$set': {
+                                 'password': request.json['password']
+                             }})
+            if (q['n'] == 0):
                 print("Update failed")
-                return jsonify({}),400
+                return jsonify({}), 400
             print("Update successful")
             return jsonify({}), 200
         else:
             print("Update failed")
-            return jsonify({}),400
-
-if __name__=='__main__':
-	app.run(debug = True,host = "0.0.0.0", port = "5000")
+            return jsonify({}), 400
 
 
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port="5000")
